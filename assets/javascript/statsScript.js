9b6c4b61323b7d4bee79b64f3e29a5df578bb331
Luke Keltner <lkeltner87@gmail.com>
2017-09-23 13:52:06 -0400

    Can switch views on graph

--- assets/javascript/statsScript.js
+++ assets/javascript/statsScript.js
@@ -2,6 +2,8 @@ var userID = -1;
 var token = sessionStorage.getItem("userID");
 var maxCoins = 0;
 var initialMaxCoins = 100;
+var coinsOverTime = [];
+var chart;
 
 
 $(function () 
@@ -9,10 +11,22 @@ $(function ()
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+var displayStars = function()
+{
+	$('#stars-container').html("")
+	database.ref("users/"+userID).once("value", function(snap)
+	{
+		var numberOfStars = snap.val().stars
+
+		for (var i=0; i<numberOfStars; i++)
+		{
+			$('#stars-container').append('<i class="fa fa-star fa-2x" aria-hidden="true"></i>')
+		}
+	})
+}
+
 database.ref("users").once('value').then(function(snap)
 {
-	var coinsOverTime = []
-
 	for (var i=0; i<snap.val().length; i++)
 	{
 		if (snap.val()[i].token === token)
@@ -20,14 +34,15 @@ database.ref("users").once('value').then(function(snap)
 			userID = i;
 			$("#name").html(snap.val()[i].name)
 			newUser = snap.val()[i].new
-			$('#coins-display').html(snap.val()[i].coins)
+			$('#coin-container').html('<h1>'+snap.val()[i].coins.toLocaleString()+'</h1>')
+			displayStars()
 
 			var stars = snap.val()[i].stars
 			maxCoins = initialMaxCoins * Math.pow(10, stars)
 			coinsOverTime = snap.val()[i].coinsOverTime
 			coinsOverTime.unshift('Coins Over Time')
 
-			var chart = c3.generate(
+			chart = c3.generate(
 			{
 			    bindto: '#chart',
 			    data: {
@@ -35,23 +50,6 @@ database.ref("users").once('value').then(function(snap)
 			        coinsOverTime,
 			      ]
 			    },
-			    grid: 
-			    {
-				    y: 
-				    {
-				        lines: 
-				        [
-				            {value: maxCoins, text: 'Next Star', position: 'start', stroke: 'green'},
-				        ]
-				    }
-		    	},
-			    axis: 
-			    {
-				    y: 
-				    {
-				        max: maxCoins
-				    }
-		    	},
 			});
 		}
 	}
@@ -107,4 +105,61 @@ database.ref("questions").once("value").then(function(snap)
 			$('#table-body').append(tr)
 		}
 	})
+})
+
+$('#switch-graph').on('click', function(event)
+{
+	var text = $('#switch-graph').html()
+	console.log(text)
+
+	if (text === "Show Next Star")
+	{
+		chart = c3.generate(
+		{
+			bindto: '#chart',
+			data: 
+			{
+				columns: 
+				[
+				    coinsOverTime,
+				]
+			},
+			grid: 
+			{
+				y: 
+				{
+					lines: 
+					[
+					    {value: maxCoins, text: 'Next Star', position: 'start', stroke: 'green'},
+					]
+				}
+			},
+			axis: 
+			{
+				y: 
+				{
+					max: maxCoins
+				}
+			},
+		});	
+
+		$('#switch-graph').html("Show Progress")
+	}
+
+	if (text === "Show Progress")
+	{
+		chart = c3.generate(
+		{
+			bindto: '#chart',
+			data: 
+			{
+				columns: 
+				[
+				    coinsOverTime,
+				]
+			},
+		});	
+
+		$('#switch-graph').html("Show Next Star")
+	}
 })
