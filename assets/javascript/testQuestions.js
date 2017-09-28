2efbf7b7605c09caa810b6a1d7c2526aeecaa5ea
Luke Keltner <LKelt@hawken.edu>
2017-09-28 08:53:08 -0400

    Added test question topic

---
+++ assets/javascript/testQuestions.js
@@ -0,0 +1,58 @@
+var questionBank = 
+[
+	{
+		type: "random",
+		numberOfRandom: 2,
+		rand0:
+		{
+			min: 10,
+			max: 80,
+			decimal: 0
+		},
+
+		rand1:
+		{
+			min: 2,
+			max: 8,
+			decimal: 2
+		},
+
+		question:"Test Question.  A ball is lauched on level ground at an angle of rand0 degrees with an intitial velocity of rand1 m/s.  How much distance does the ball cover by the time it hits the ground?",
+		correct:"Math.pow(rand1,2)*Math.sin(2*rand0*Math.PI/180)/10",
+		wrong:["rand0*rand1", "rand0+rand1", "rand0/rand1"]
+	},
+
+	{
+		type: "random",
+		numberOfRandom: 3,
+		rand0:
+		{
+			min: 6,
+			max: 80,
+			decimal: 2
+		},
+
+		rand1:
+		{
+			min: 2,
+			max: 7,
+			decimal: 2
+		},
+
+		rand2:
+		{
+			min: 2,
+			max: 7,
+			decimal: 2
+		},
+
+		question:"Test Question.  What is rand1 squared times sin(rand0) divided by rand2?",
+		correct:"(Math.pow(rand1,2)*Math.sin(rand0*Math.PI/180))/rand2",
+		wrong:["rand0", "rand1", "rand0+rand1"]
+	},
+]
+
+database.ref("questions").update(
+{
+	"Test Questions": questionBank
+})
