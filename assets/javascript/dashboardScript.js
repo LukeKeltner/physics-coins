d53902a7930ce0cf0c60b8c4aa262423e446276b
Luke Keltner <lkeltner87@gmail.com>
2017-09-30 10:03:36 -0400

    Added calculator

--- assets/javascript/dashboardScript.js
+++ assets/javascript/dashboardScript.js
@@ -10,6 +10,7 @@ var wrong = new Audio('assets/sounds/wrong.mp3');
 var gotStar = new Audio('assets/sounds/star.mp3');
 var questionNumberCorrect = 0;
 var initialMaxCoins = 100
+var calcOpen = false;
 
 if (token === null)
 {
@@ -364,4 +365,98 @@ $('.gamble-amount-button').on('click', function(event)
 		gambleAmountDisplay.html("Pick an Amount of Coins")
 		$('#go-for-it').prop('disabled', true);
 	}
+})
+
+
+
+
+
+updateScreen = function(displayValue) {
+
+  var displayValue = displayValue.toString();
+  $('.screen').html(displayValue.substring(0, 10));
+};
+
+  var result = 0;
+  var prevEntry = 0;
+  var operation = null;
+  var currentEntry = '0';
+  updateScreen(result);
+  
+  $('.calc-button').on('click', function(evt) {
+    var buttonPressed = $(this).html();
+    console.log(buttonPressed);
+    
+    if (buttonPressed === "C") {
+      result = 0;
+      currentEntry = '0';
+    } else if (buttonPressed === "CE") {
+      currentEntry = '0';
+    } else if (buttonPressed === "back") {
+      //currentEntry = currentEntry.substring(0, currentEntry.length-1);
+    } else if (buttonPressed === "+/-") {
+      currentEntry *= -1;
+    } else if (buttonPressed === '.') {
+      currentEntry += '.';
+    } else if (isNumber(buttonPressed)) {
+      if (currentEntry === '0') currentEntry = buttonPressed;
+      else currentEntry = currentEntry + buttonPressed;
+    } else if (isOperator(buttonPressed)) {
+      prevEntry = parseFloat(currentEntry);
+      operation = buttonPressed;
+      currentEntry = '';
+    } else if(buttonPressed === '%') {
+      currentEntry = currentEntry / 100;
+    } else if (buttonPressed === 'sqrt') {
+      currentEntry = Math.sqrt(currentEntry);
+    } else if (buttonPressed === '1/x') {
+      currentEntry = 1 / currentEntry;
+    } else if (buttonPressed === 'pi') {
+      currentEntry = Math.PI;
+    } else if (buttonPressed === '=') {
+      currentEntry = operate(prevEntry, currentEntry, operation);
+      operation = null;
+    }
+    
+    updateScreen(currentEntry);
+  });
+
+
+
+isNumber = function(value) {
+  return !isNaN(value);
+}
+
+isOperator = function(value) {
+  return value === '/' || value === '*' || value === '+' || value === '-';
+};
+
+operate = function(a, b, operation) {
+  a = parseFloat(a);
+  b = parseFloat(b);
+  console.log(a, b, operation);
+  if (operation === '+') return a + b;
+  if (operation === '-') return a - b;
+  if (operation === '*') return a * b;
+  if (operation === '/') return a / b;
+}
+
+$('#show-calc').on("click", function(event)
+{
+	if(!calcOpen)
+	{
+		$('.calculator-div').show()
+		$('.question-div').attr('class', 'col-xs-6 question-div')
+		$('#show-calc').html("Hide Calculator")
+		calcOpen = true;	
+	}
+
+	else if (calcOpen)
+	{
+		$('.calculator-div').hide()
+		$('.question-div').attr('class', 'col-xs-12 question-div')
+		$('#show-calc').html("Show Calculator")
+		calcOpen = false;			
+	}
+
 })
